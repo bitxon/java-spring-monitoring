57dc8f6cc469fa0695c34d544e91095cfba2a25d
Nikita Karpuk <bitxon.dev@gmail.com>
2025-09-03 11:34:40 +0200

    Upgrade Spring 3.5.5

--- order-service/src/main/java/bitxon/order/config/TracingConfig.java
+++ order-service/src/main/java/bitxon/order/config/TracingConfig.java
@@ -1,5 +1,8 @@
 package bitxon.order.config;
 
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import io.micrometer.tracing.exporter.SpanExportingPredicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +10,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TracingConfig {
 
+    /**
+     * ObservedAspect makes @Observed annotation work
+     */
     @Bean
-    SpanExportingPredicate noActuator() {
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+
+    @Bean
+    SpanExportingPredicate noActuatorSpansForZipkin() {
         return span -> span.getTags().get("uri") == null || !span.getTags().get("uri").startsWith("/actuator");
+    }
+
+    @Bean
+    MeterFilter noActuatorMetricsForPrometheus() {
+        return MeterFilter.deny(id -> id.getTag("uri") != null && id.getTag("uri").startsWith("/actuator"));
     }
 }
