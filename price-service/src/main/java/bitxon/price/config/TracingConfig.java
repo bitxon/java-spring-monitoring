61cf8b2b7c8c4a90a7daaeba08b4a925c0670277
Nikita Karpuk <bitxon.dev@gmail.com>
2025-09-03 11:34:40 +0200

    Upgrade Spring 3.5.5

--- price-service/src/main/java/bitxon/price/config/TracingConfig.java
+++ price-service/src/main/java/bitxon/price/config/TracingConfig.java
@@ -1,5 +1,6 @@
 package bitxon.price.config;
 
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.tracing.exporter.SpanExportingPredicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class TracingConfig {
 
     @Bean
-    SpanExportingPredicate noActuator() {
+    SpanExportingPredicate noActuatorSpansForZipkin() {
         return span -> span.getTags().get("uri") == null || !span.getTags().get("uri").startsWith("/actuator");
+    }
+
+    @Bean
+    MeterFilter noActuatorMetricsForPrometheus() {
+        return MeterFilter.deny(id -> id.getTag("uri") != null && id.getTag("uri").startsWith("/actuator"));
     }
 }
